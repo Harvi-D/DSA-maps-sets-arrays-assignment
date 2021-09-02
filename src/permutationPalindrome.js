0d7a40b3b003463224ca5a9ad535dc135f7fddc4
Dylan Harville <dr.dyloh@gmail.com>
2021-09-02 16:37:00 -0600

    tests for firstSingleCharacter function passing

--- src/permutationPalindrome.js
+++ src/permutationPalindrome.js
@@ -2,6 +2,16 @@
  * Return true if some permutation of this word is a palindrome
  * @param {string} word The word to check
  */
-function permutationPalindrome(word) {}
+function permutationPalindrome(word) {
+    //check for palindrome
+    const isPalindrome = (word) => {
+        const wordArray = Array.from(word)
+        const forward = wordArray.toLowerCase().join('');
+        const backward = wordArray.toLowerCase().split('').reverse().join('');
+
+        (forward === backward) ? true : false;
+    }
+    console.log(isPalindrome('Hello'))
+}
 
 module.exports = permutationPalindrome;
